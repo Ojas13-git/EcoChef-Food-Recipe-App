7f0a43c14f407076558b8a1f499a110906295230
Ojas13-git <79032848+Ojas13-git@users.noreply.github.com>
2023-12-31 20:38:46 +0530

    backend over

--- server/routes.js
+++ server/routes.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const { signup, login, logout, checkUser } = require("./controllers/authController");
 const { verifyToken } = require("./middlewares/verifyToken");
+const {
+    addToFavourites,
+    removeFromFavourites,
+    getFavourties,
+  } = require("./controllers/featureController");
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.get("/logout", logout);
 router.get("/checkUser",verifyToken, checkUser);
+router.post("/addToFavourites/:id", addToFavourites);
+router.post("/removeFromFavourites/:id", removeFromFavourites);
+router.get("/getFavourites/:id", getFavourties);
 
 module.exports = router;
