de5a99136b476cf6251bea892d0caa0cf73115ae
Ojas13-git <79032848+Ojas13-git@users.noreply.github.com>
2023-12-31 19:12:35 +0530

    added favourite featureController

--- server/routes.js
+++ server/routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { signup, login, logout } = require("./controllers/authController");
+const { signup, login, logout, checkUser } = require("./controllers/authController");
+const { verifyToken } = require("./middlewares/verifyToken");
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.get("/logout", logout);
+router.get("/checkUser",verifyToken, checkUser);
 
 module.exports = router;
